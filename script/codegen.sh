3ba869cd9c9380779f30ab950d66f2442d3e61ea
jiaw127 <jiaw127@163.com>
2023-08-10 21:13:53 +0800

    update api spec
    Signed-off-by: jiaw127 <jiaw127@163.com>

--- script/codegen.sh
+++ script/codegen.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
 # step 1:  go get github.com/deepmap/oapi-codegen/cmd/oapi-codegen
-# step 2: run produce
+# step 2: check output exist
+home=$(pwd)
+models=$home/pkg/models
+handler=$home/pkg/handler
+if [ ! -d "$models" ]; then
+  mkdir -p $models
+fi
+if [ ! -d "$handler" ]; then
+  mkdir -p $handler
+fi
+# step 3: gen code
 oapi-codegen --config=script/module-config.yaml api/api.yaml
 oapi-codegen --config=script/server-config.yaml api/api.yaml
