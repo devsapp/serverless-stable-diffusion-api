cfb364bd884000b3fedeeff83ce3058d5b4563b8
jiaw127 <jiaw127@163.com>
2023-08-11 10:50:25 +0800

    add client yaml
    Signed-off-by: jiaw127 <jiaw127@163.com>

--- script/codegen.sh
+++ script/codegen.sh
@@ -4,12 +4,17 @@
 home=$(pwd)
 models=$home/pkg/models
 handler=$home/pkg/handler
+client=$home/pkg/client
 if [ ! -d "$models" ]; then
   mkdir -p $models
 fi
 if [ ! -d "$handler" ]; then
   mkdir -p $handler
 fi
+if [ ! -d "$client" ]; then
+  mkdir -p $client
+fi
 # step 3: gen code
 oapi-codegen --config=script/models-config.yaml api/api.yaml
 oapi-codegen --config=script/server-config.yaml api/api.yaml
+oapi-codegen --config=script/client-config.yaml api/api.yaml
