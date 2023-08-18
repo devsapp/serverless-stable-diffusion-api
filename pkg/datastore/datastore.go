88c19d2da91ef0f2f2786573b141fa99513e1237
jiaw127 <jiaw127@163.com>
2023-08-18 16:59:06 +0800

    fix comments with datastore
    Signed-off-by: jiaw127 <jiaw127@163.com>

--- pkg/datastore/datastore.go
+++ pkg/datastore/datastore.go
@@ -23,7 +23,8 @@ type Datastore interface {
 	// It takes a key and a map of column names to values, and returns an error if the operation failed.
 	Put(key string, values map[string]interface{}) error
 
-	// Update the column values in the datastore
+	// Update the partial column values.
+	// It tasks a key and a map of column names to values, and returns an error if the operation failed.
 	Update(key string, values map[string]interface{}) error
 
 	// Get retrieves the column values from the datastore.
@@ -41,7 +42,7 @@ type Datastore interface {
 	Delete(key string) error
 
 	// ListAll read all data from the datastore.
-	// It return a nested map, which means map[primaryKey]map[columanName]columanValue.
+	// It takes a list of column name, and  return a nested map, which means map[primaryKey]map[columanName]columanValue.
 	// Note: since it reads all data and store them in memory, so do not call this function on a large datastore.
 	ListAll(columns []string) (map[string]map[string]interface{}, error)
 
